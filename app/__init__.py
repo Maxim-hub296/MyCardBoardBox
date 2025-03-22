2c2809990c6ef8e13b469af09a0a2ca8cb65ac3d
Maxim Savikin <maxsavikin@gmail.com>
2025-03-22 15:32:51 +0300

    Добавил форму авторизации (на будущее, пока не используется)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+from secrets import token_hex
+
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = token_hex(32)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-
+from app.forms import LoginForm
 @app.route("/")
 def about():
     return render_template("about.html", title_name="О нас")
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Пароль получен')
+        print(form.password.data)
+        return redirect('/')
+    return render_template('login.html', title_name="Кто это пытается дополнить мой сайт?", form=form)
